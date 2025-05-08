d720a86944ed9f7056f359fa3205486407021f04
пк <pk@MacBook.local>
2025-05-08 19:09:53 +0500

    New version of Main & Control Tower

--- Task2/ControlTower.java
+++ Task2/ControlTower.java
@@ -1,15 +1,15 @@
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class ControlTower implements TowerMediator {
-    private LinkedList<Aircraft> landingQueue = new LinkedList<>(); //changing to LinkedList
-    private LinkedList<Aircraft> takeoffQueue = new LinkedList<>();
+    private Queue<Aircraft> landingQueue = new LinkedList<>();
+    private Queue<Aircraft> takeoffQueue = new LinkedList<>();
     private boolean runwayOccupied = false;
     private boolean emergencyMode = false;
 
     @Override
     public void broadcast(String msg, Aircraft sender) {
         System.out.println("Tower: [" + sender.getId() + "] says: " + msg);
-        //echo to all aircraft except sender
         for (Aircraft a : landingQueue) {
             if (a != sender) a.receive(msg);
         }
@@ -19,22 +19,21 @@ public class ControlTower implements TowerMediator {
     }
 
     @Override
-    public boolean requestRunway(Aircraft a, String msg) {
+    public boolean requestRunway(Aircraft a) {
         if (msg.contains("MAYDAY")) {
             handleEmergency(a);
             return true;
         }
         if (msg.contains("LAND")) {
             if (a.getFuelLevel() < 20) {
-                landingQueue.add(0, a);
-                broadcast("Low fuel priority for " + a.getId(), a);
+                landingQueue.add(0, a); //low fuel priority
+                broadcast("Low fuel priority for " + a.getId(), this);
             } else {
                 landingQueue.add(a);
             }
         } else if (msg.contains("TAKEOFF")) {
             takeoffQueue.add(a);
         }
-        //try to grant runway
         return tryGrantRunway();
     }
 
@@ -43,7 +42,7 @@ public class ControlTower implements TowerMediator {
         runwayOccupied = true;
         landingQueue.clear();
         takeoffQueue.clear();
-        broadcast("MAYDAY! All aircraft hold position", a);
+        broadcast("MAYDAY! All aircraft hold position", this);
         System.out.println("Tower: " + a.getId() + " cleared for emergency landing");
         //mayday gets runway
     }
@@ -70,6 +69,6 @@ public class ControlTower implements TowerMediator {
         runwayOccupied = false;
         emergencyMode = false;
         tryGrantRunway();
-        //free runway and check queues
+        //reset for next
     }
 }
--- Task2/Main.java
+++ Task2/Main.java
@@ -1,16 +1,42 @@
+import java.util.Random;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         ControlTower tower = new ControlTower();
-        Aircraft p1 = new PassengerPlane("PS123", 15); // low fuel
-        Aircraft c1 = new CargoPlane("CG456", 60);
-        Aircraft h1 = new Helicopter("HC789", 50);
+        Random rand = new Random();
+        Aircraft[] aircraft = new Aircraft[10];
 
-        p1.send("Request LAND", tower); // low fuel priority
-        c1.send("Request TAKEOFF", tower);
-        h1.send("Request MAYDAY", tower); // emergency
+        //spawn 10 random aircraft
+        for (int i = 0; i < 10; i++) {
+            int fuel = rand.nextInt(100) + 1;
+            int type = rand.nextInt(3);
+            String id = "AC" + i;
+            if (type == 0) aircraft[i] = new PassengerPlane(id, fuel);
+            else if (type == 1) aircraft[i] = new CargoPlane(id, fuel);
+            else aircraft[i] = new Helicopter(id, fuel);
+        }
 
-        tower.releaseRunway();
-        tower.releaseRunway();
-        //manual test
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        for (Aircraft a : aircraft) {
+            scheduler.scheduleAtFixedRate(() -> {
+                String action = rand.nextInt(10) < 8 ? "LAND" : "TAKEOFF";
+                if (rand.nextInt(100) < 5) action = "MAYDAY"; //5% chance
+                a.send("Request " + action, tower);
+                tower.releaseRunway();
+            }, rand.nextInt(5), 1, TimeUnit.SECONDS);
+        }
+
+        //run for 10 seconds
+        try {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            scheduler.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //simulate traffic
     }
 }
